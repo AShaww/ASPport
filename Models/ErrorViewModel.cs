03dfa5c019bf74a06bdac81893653e5df926c0d9
AShaww <71328022+AShaww@users.noreply.github.com>
2021-08-16 21:55:22 +0100

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ASPort.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
