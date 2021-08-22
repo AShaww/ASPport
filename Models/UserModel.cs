4a9314bd2d9358b3cc8c50203ae2556dab201393
AShaww <71328022+AShaww@users.noreply.github.com>
2021-08-22 22:41:22 +0100

    Added Login View, with controller. This will be used to verify user details to log into "Dashboard"

---
+++ Models/UserModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Threading.Tasks;
+
+namespace ASPort.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
